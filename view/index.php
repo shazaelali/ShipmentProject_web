474418921f8416829b98de11c4fe44438e657c98
shazaElali <shaza.el-ali@isae.edu.lb>
2020-05-12 14:13:56 +0300

    3 commit

---
+++ view/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+<title>shipping.com</title>
+</head>
+<body>
+<nav>
+
+</nav>
+ssfsd
+
+
+
+</body>
+</html>      
