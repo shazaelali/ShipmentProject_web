bba7a7a7f412dbb176da8dcbbaad577bc3e25e45
shazaElali <shaza.el-ali@isae.edu.lb>
2020-06-27 02:18:20 +0300

    commit

---
+++ admin.php
@@ -0,0 +1,182 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+   <!-- <link rel="stylesheet" type="text/css" href="css/signupStyle.css">
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">-->
+    
+   <!--<script> src="javaScript.js"</script>-->
+ 
+  
+   <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+  
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<link rel="stylesheet"  href="css/admin.css">
+       <title>Document</title>
+</head>
+<body >
+  <div class="d-flex">
+      <div id="sidbar-container" class=" btn-pramiry">
+          <div class="logo ">
+             <h4 class="text-light font-weight-bold  m-2">Admin</h4>
+          </div>
+            <div class="menu">
+                <a href="#" class="d-block  text-light p-3 " ><i class="icon ion-md-apps"></i>dashboard</a>
+                <a href="# " class="d-block  text-light p-3"><i class="icon ion-md-people"></i>rhgkn</a>
+                <a href="#" class="d-block text-light p-3"><i class="icon ion-ios-stats"></i>vnrnh</a>
+            </div>
+      </div>
+      <div class="w-100">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+       <ul class="navbar-nav ml-auto">
+      
+      <li class="nav-item dropdown">
+         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         <img src="images/icon-delivery.png" alt="" class="img-fluid rounded-circle img mr-2"> Dropdown
+         </a>
+         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">My Profile</a>
+          <a class="dropdown-item" href="#"></a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">logOut</a>
+        </div>
+      </li>
+    
+    </ul>
+  
+    </div>
+    </div>
+  </nav>
+       <div id="content">
+       <div class="container-fluid" style="height:100%;overflow:auto;background-color:#F5F5F5">
+
+         <div class="row">
+
+             <div class="col-12">
+                  <div class="row">
+                     <div class="col-6">
+                       <div style="margin: 20px;">
+                   
+                       </div>
+                     </div>
+                       <div class="col-6">
+
+                       <div style="margin: 20px;">
+
+                  
+                   
+                       </div>
+
+                
+                    </div>
+              </div>
+          </div>
+
+        </div>
+<div class="row">
+    <div class="col-lg-8 col-12">
+       
+      
+
+        <div class="row" >
+
+            <div class="col-sm-12 col-lg-12" style="">
+                <div class="chart-container" style="background-color:white ;border-radius: 5px;padding:15px;margin-bottom:20px;">
+                   
+
+                    <div class="card-body">
+
+
+                        <div id="bar-example" style=" height: 220px;"></div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+
+
+        <div class="row">
+
+            <div class="col-sm-12 col-lg-12">
+                <div class="chart-container" style="background-color:white ;border-radius: 5px;padding:15px;margin-bottom:20px;">
+                   
+
+                    <div class="card-body">
+
+
+                        <div id="bar-example" style=" height: 220px;"></div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+        <div class="row" >
+
+            <div class="col-sm-12 col-lg-12" style="">
+                <div class="chart-container" style="background-color:white ;border-radius: 5px;padding:15px;margin-bottom:20px;">
+                  
+
+                    <div class="card-body">
+
+
+                        <div id="bar-example" style=" height: 220px;"></div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+      
+    </div>
+
+    <div class="col-lg-4 col-12">
+
+
+        <div class="row " style="min-height:318px; background-color:white; border-radius: 5px; margin:0px 5px;margin-bottom:20px;">
+           
+
+
+        </div>
+
+        <div class="row " style="min-height:318px; background-color:white; border-radius: 5px; margin:0px 5px;margin-bottom:20px;">
+           
+
+
+        </div>
+
+
+   
+
+        
+    </div>
+
+</div>
+</div>
+
+       </div>
+
+      </div>
+  </div>  
+
+
+
+</body>
+</html>
