a717df153e006debbd8c1c8e5d4cdd94679ccd44
shazaElali <shaza.el-ali@isae.edu.lb>
2020-08-12 14:36:59 +0300

    commit13

--- admin.php
+++ admin.php
@@ -8,11 +8,10 @@
 
          $res=mysqli_query($connect,$q);
 
-         // while($row=mysqli_fetch_assoc($res))
-          // {
-           // echo'
-                   //<option values="'.$row['IdRegion'].'">'.$row['regionName'].'</option>';
-                   // }
+         $query = "SELECT Account, count(*) as number FROM users GROUP BY Account";  
+         $result = mysqli_query($connect, $query);  
+         $qu = "SELECT Address, count(*) as number FROM users GROUP BY Address";  
+         $resulta = mysqli_query($connect, $qu);  
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -204,17 +203,30 @@
                                     <div class="row">
 
                                         <div class="col-sm-12 col-lg-12">
-                                            <div class="formDiv">
+                                             <div class="row">
+                                                 <div class="col-md-6 mb-3 div"> 
+                                                        <div class="formDiv">
 
 
-                                                <div  class="heightDiv">
+                                                           <div id="piechart" style="width: 400px; height: 350px;">
+                                                           </div> 
+                                                     </div>
+                                                  </div>
+                                                  <div class="col-md-6 mb-3 div"> 
+                                                        <div class="formDiv">
 
+ 
+                                                            <div  class="heightDiv">
 
+                                                           
                                                    
 
-                                                </div>
-                                            </div>
-
+                                                             </div>
+                                                        </div>
+                                                  </div>
+                                              
+                                              </div>
+                                              
 
                                         </div>
                                     </div>
@@ -226,11 +238,11 @@
 
 
 
-                                            <div  class="heightDiv"> 
+                                            
 
-
+                                              <div id="top_x_div" style="width: 300px; height: 300px;"></div>
                                                    
-                                            </div>
+                                            
 
 
                                         </div>
@@ -253,6 +265,70 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+           
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Admin', 'User'],
+          <?php  
+                          while($row = mysqli_fetch_array($resulta))  
+                          {  
+                               echo "['".$row["Address"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+      
+        ]);
+
+      var options = {
+        legend: 'none',
+        pieSliceText: 'label',
+        title: 'Swiss Language Use (100 degree rotation)',
+        pieStartAngle: 100,
+        pieHole: 0.4 
+      };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+    </script> 
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawStuff);
+
+      function drawStuff() {
+        var data = new google.visualization.arrayToDataTable([
+            ['Admin', 'User'],
+          <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["Account"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+        ]);
+
+        var options = {
+          title: 'Chess opening moves',
+          width: 900,
+          legend: { position: 'none' },
+          chart: { title: 'Chess opening moves',
+                   subtitle: 'popularity by percentage' },
+          bars: 'horizontal', // Required for Material Bar Charts.
+          axes: {
+            x: {
+              0: { side: 'top', label: 'Percentage'} // Top x-axis.
+            }
+          },
+          bar: { groupWidth: "90%" }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('top_x_div'));
+        chart.draw(data, options);
+      };
+    </script>
 
 
 
